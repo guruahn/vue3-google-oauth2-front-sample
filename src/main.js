f4f1e267f6f1b3a2dfc7259f3ded3e668068c4a8
Jeong woo Ahn <guruahn@gmail.com>
2020-12-06 01:31:20 +0900

    gauthClientId settiing error fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './index.css'
 import gAuthPlugin from 'vue3-google-oauth2';
 const app = createApp(App)
-const gauthClientId = import.meta.env.VITE_GAUTH_CLIENT_ID ? import.meta.env.VITE_GAUTH_CLIENT_ID : process.env.VITE_GAUTH_CLIENT_ID
+let gauthClientId = import.meta.env.VITE_GAUTH_CLIENT_ID
+if(!gauthClientId) gauthClientId = process.env.VITE_GAUTH_CLIENT_ID
 app.use(gAuthPlugin, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: false })
 
 app.mount('#app')
