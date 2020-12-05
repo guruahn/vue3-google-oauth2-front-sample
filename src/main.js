716ca45c74676e79dab1d36bc41b3528b6d71269
Jeong woo Ahn <guruahn@gmail.com>
2020-12-06 01:32:38 +0900

    test gauthClientId

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import gAuthPlugin from 'vue3-google-oauth2';
 const app = createApp(App)
 let gauthClientId = import.meta.env.VITE_GAUTH_CLIENT_ID
 if(!gauthClientId) gauthClientId = process.env.VITE_GAUTH_CLIENT_ID
+console.log(gauthClientId)
 app.use(gAuthPlugin, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: false })
 
 app.mount('#app')
