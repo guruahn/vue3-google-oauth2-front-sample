d251ef1a5585b0a6ca08357d6a790784bc21006f
Jeong woo Ahn <guruahn@gmail.com>
2020-12-06 01:17:00 +0900

    refactoring view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,88 @@
 <template>
 <div>
-  <h1>googleAuthIsInit: {{ Vue3GoogleOauth.isInit }}</h1>
-  <button @click="handleClickSignIn" :disabled="!Vue3GoogleOauth.isInit">sign in</button>
-  <button @click="handleClickLogin" :disabled="!Vue3GoogleOauth.isInit">get authCode</button>
+  <h1>IsInit: {{ Vue3GoogleOauth.isInit }}</h1>
+  <h1>IsAuthorized: {{ Vue3GoogleOauth.isAuthorized }}</h1>
+  <h2 v-if="user">signed user: {{user}}</h2>
+  <button @click="handleClickSignIn" :disabled="!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized">sign in</button>
+  <button @click="handleClickGetAuthCode" :disabled="!Vue3GoogleOauth.isInit">get authCode</button>
+  <button @click="handleClickSignOut" :disabled="!Vue3GoogleOauth.isAuthorized">sign out</button>
+  <button @click="handleClickDisconnect" :disabled="!Vue3GoogleOauth.isAuthorized">disconnect</button>
 </div>
 </template>
 
 <script>
-import {
-  ref,
-  reactive,
-  inject
-} from "vue";
+import { inject, toRefs } from "vue";
 
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
+
+  data(){
+    return {
+      user: '',
+    }
+  },
+
+  methods: {
+    async handleClickSignIn(){
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        if (!googleUser) {
+          return null;
+        }
+        console.log("googleUser", googleUser);
+        this.user = googleUser.getBasicProfile().getEmail();
+        console.log("getId", this.user);
+        console.log("getBasicProfile", googleUser.getBasicProfile());
+        console.log("getAuthResponse", googleUser.getAuthResponse());
+        console.log(
+          "getAuthResponse",
+          this.$gAuth.instance.currentUser.get().getAuthResponse()
+        );
+
+      } catch (error) {
+        //on fail do something
+        console.error(error);
+        return null;
+      }
+    },
+
+    async handleClickGetAuthCode(){
+      try {
+        const authCode = await this.$gAuth.getAuthCode();
+        console.log("authCode", authCode);
+      } catch(error) {
+        //on fail do something
+        console.error(error);
+        return null;
+      }
+    },
+
+    async handleClickSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        console.log("isAuthorized", this.Vue3GoogleOauth.isAuthorized);
+        this.user = "";
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    handleClickDisconnect() {
+      window.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.href}`;
+    },
+  },
   setup(props) {
-    console.log("hello");
+    const { isSignIn } = toRefs(props);
     const Vue3GoogleOauth = inject("Vue3GoogleOauth");
-    console.log("Vue3GoogleOauth", Vue3GoogleOauth);
 
     const handleClickLogin = () => {};
-    const handleClickSignIn = () => {};
     return {
       Vue3GoogleOauth,
       handleClickLogin,
-      handleClickSignIn,
+      isSignIn,
     };
   },
 };
@@ -55,6 +109,7 @@ button {
   padding: 12px 20px;
   font-size: 14px;
   border-radius: 4px;
+  margin-right: 1em;
 }
 
 button:disabled {
