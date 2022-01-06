153d87538476f629a634213c0adf71577d01a69a
Jeongwoo Ahn <guruahn@gmail.com>
2022-01-06 12:31:35 +0900

    ⬆️ update dependencies

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,6 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
